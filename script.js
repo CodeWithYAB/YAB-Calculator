b6d5bf88e06702a67c71449f4eefacbd999c84f2
CodeWithYAB <100062630+CodeWithYAB@users.noreply.github.com>
2022-04-19 17:55:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
-// Variables
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
-
-// Logics
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
-        
-        // Multiplication Logic
+        console.log('Button text is ', buttonText);
         if (buttonText == '×') {
             buttonText = '*';
             screenValue += buttonText;
             screen.value = screenValue;
         }
-
-        // Division Logic
         if (buttonText == '÷') {
             buttonText = '/';
             screenValue += buttonText;
             screen.value = screenValue;
         }
-
-        // Clear All Logic
         else if (buttonText == 'C') {
             screenValue = "";
             screen.value = screenValue;
         }
-
-        // = Logic
         else if (buttonText == '=') {
             screen.value = eval(screenValue);
         }
-        
         else {
             screenValue += buttonText;
             screen.value = screenValue;
@@ -40,3 +29,4 @@ for (item of buttons) {
 
     })
 }
+
